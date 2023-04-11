14b5104bc48b61c28da0da4dc5fc968cbe3db402
SeungJin Nam <nsj2204@gmail.com>
2023-04-12 00:45:57 +0900

    09~10 Chap

---
+++ 09_Operator/09_Operator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Operator
+{
+    static void Main(string[] args)
+    {
+        // 연산자(Operator) : 데이터와 변수를 가지고 더하기(+), 빼기(-)와 같은 연산 작업을 수행
+        var num = 1000;
+        var number = num + 1234;
+        Console.WriteLine(number);
+
+        // [?] 식(Expression)
+
+        // [?] 문(Statement)
+        // Console.log(3*5);
+
+        // [?] 단항 연산자(Unary Operator): +, -
+        var value = 0;
+        Console.WriteLine(value);
+        value = 8;
+        value = +value;
+        Console.WriteLine(value);
+        value = -value;
+        Console.WriteLine(value);
+
+        Console.WriteLine("Hello, " + "World");
+        Console.WriteLine("123" + "456");
+        Console.WriteLine(123+456);
+        Console.WriteLine("123"+456);
+
+    }
+}
---
+++ 10_AssignmentOperator/10_AssignmentOperator/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace _10_AssignmentOperator
+{
+    class AssignmentOperator
+    {
+        static void Main(string[] args)
+        {
+            // [?] 할당(대입) 연산자: 변수에 특정 값으로 초기화하면 자동 유추에 의해서 형식이 결정됨
+            var name = "Java";
+            var version = 14;
+            Console.WriteLine(name + " " + version);
+
+            var i = 100;
+            var j = 200;
+            Console.WriteLine("처음: " + i + ", " + j);
+
+            // 변수값 서로 바꾸기
+            var temp = i;
+            i = j;
+            j = temp;
+            Console.WriteLine("변경: " + i + ", " + j);
+
+            // [?] 정수형 변수의 값을 1씩 증가
+            int num = 10;
+            num += 1; // 1 증가
+            Console.WriteLine(num);
+            num -= 1; // 1 감소
+            Console.WriteLine(num);
+
+            int x = 3;
+            int y = 3;
+
+            x = x + 2; //기본형
+            y += 2;    //축약형
+            Console.WriteLine(x + ", " + y);
+
+            int a = 3;
+            int b = 5;
+            b += a; // 누적: 변수 b에 a변수의 값을 누적
+            Console.WriteLine(b);
+            b -= a;
+            Console.WriteLine(b);
+
+            int numb = 100;
+            // numb = numb+1;
+            // numb += 1;
+            ++numb;
+            Console.WriteLine(numb);
+
+
+            int numm = -100;
+            // numm = numm-1;
+            // numm -= 1;
+            --numm;
+            Console.WriteLine(numm);
+
+            // 전위(prefix) 증감 연산자: 우선순위가 높음
+            int q = 3;
+            int w = ++q;
+            Console.WriteLine(w); //4
+
+            // 후위(postfix) 증감 연산자: 우선순위가 낮음
+            int v = 3;
+            int z = v++;
+            Console.WriteLine(z); //3
+            Console.WriteLine(v); //4
+
+            // 연습문제: 증가, 감소 연산자 사용하기
+            int number1 = 3;
+            int number2 = 5;
+
+            int result1 = 0;
+            int result2 = 0;
+
+            number1--; // 2
+            result1 = ++number1; // 3
+
+            ++number2; // 6
+            result2 = number2--; // 6
+
+            Console.WriteLine(result1); // 3
+            Console.WriteLine(result2); // 6
+        }
+    }
+}

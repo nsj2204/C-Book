dd680364d67f4b6f70cbefc37428ff16cca344a4
nsj2204 <129812135+nsj2204@users.noreply.github.com>
2023-04-20 16:53:01 +0900

    C# 교과서 마스터하기 57. 인메모리 데이터베이스 프로그래밍 맛보기 - C# 강의, C# 강좌, C# 동영상

---
+++ C#/57.TodoApp/TodoApp.Models/ITodoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TodoApp.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(Todo model); // 입력
+        List<Todo> GetAll();  // 출력
+
+    }
+
+}

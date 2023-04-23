9ded75c4396871fff795d6a278381ec1c8333857
nsj2204 <129812135+nsj2204@users.noreply.github.com>
2023-04-24 08:49:26 +0900

    C# 교과서 마스터하기 62. 모던 C#

---
+++ C#/62.SeeSharpNine/62.SeeSharpNine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+int FibOld(int i)
+{
+    if (i <= 2)
+    {
+        return 1;
+    }
+    return Fib(i - 2) + Fib(i - 1);
+}
+
+
+int Fib(int i) =>
+    i switch
+    {
+        int when i <= 2 => 1,
+        _ => Fib(i - 2) + Fib(i - 1)
+    };
+    
+
+Console.WriteLine($"20번째: {Fib(20)}");
+Console.WriteLine($"7번째: {Fib(7)}");
+        
